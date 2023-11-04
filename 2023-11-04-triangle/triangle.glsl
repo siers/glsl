6bba4e689409e9a9a7d2c9f4b1fb1b62baf1f07c
siers <wimuan@gmail.com>
2023-11-04 13:00:54 +0200

    triangle-cross

--- 2023-11-04-triangle/triangle.glsl
+++ 2023-11-04-triangle/triangle.glsl
@@ -12,20 +12,26 @@
 // * if it's negative, then the plane is being created on the "other side", so it's negative
 // then check that all cross products are positive, so they're on the same side of all sides
 
+// cross-clamp
+float f(vec2 a, vec2 b) {
+  return clamp(cross(vec3(a, 0.), vec3(b, 0.)), 0., 1.);
+}
 
 float sdTriangle(in vec2 p1, in vec2 p2, in vec2 p3, in vec2 p)
 {
   vec2 e0 = p2 - p1;  vec2 e1 = p3 - p2;  vec2 e2 = p1 - p3;
   vec2 v0 = p - p1;   vec2 v1 = p - p2;   vec2 v2 = p - p3;
 
-  vec3 c0 = cross(vec3(p1 - p3, 0.), vec3(p - p1, 0.));
-  vec3 c1 = vec3(p - p1, 0.);
-  vec3 c2 = vec3(p - p1, 0.);
+  // float d = min(min(v0.x*e0.y-v0.y*e0.x, v1.x*e1.y-v1.y*e1.x), v2.x*e2.y-v2.y*e2.x);
 
-  float d = min(min(v0.x*e0.y-v0.y*e0.x, v1.x*e1.y-v1.y*e1.x), v2.x*e2.y-v2.y*e2.x);
-  //float d = v0.x*e0.y-v0.y*e0.x;
+  float c1 = cross(vec3(v0, 0.), vec3(e0, 0.)).z;
+  float c2 = cross(vec3(v1, 0.), vec3(e1, 0.)).z;
+  float c3 = cross(vec3(v2, 0.), vec3(e2, 0.)).z;
+  float d = min(c1, min(c2, c3));
 
-  return clamp(0., 1., d);
+  //float gc = pow(d, 2.2); // gamma correction doesn't seem to be required
+  float gc = d;
+  return clamp(gc, 0., 1.);
 }
 
 vec4 triangle(in vec2 p1, in vec2 p2, in vec2 p3, in vec2 pos
@@ -48,10 +54,9 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 
   float rd = 3.141 * 2.;
   vec2 v = vec2(0, rd / 4.0);
-  float t = (rd + iTime) / 3.0;
-  vec2 p1 = cos(v + t * 0.);
-  vec2 p2 = cos(v + t * 1.);
-  vec2 p3 = cos(v + t * 2.);
+  vec2 p1 = cos(v + rd * (iTime + 0.) / 3.0);
+  vec2 p2 = cos(v + rd * (iTime + 1.) / 3.0);
+  vec2 p3 = cos(v + rd * (iTime + 2.) / 3.0);
 
   col = triangle(p1, p2, p3, uv, col, fill, vec4(0.), 0.);
 
