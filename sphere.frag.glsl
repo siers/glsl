0447c63850ed433546619f124d3e7c75c27bd4cf
siers <wimuan@gmail.com>
2023-11-07 15:16:52 +0200

    add glslls + rename for lsp recognition

--- sphere.frag.glsl
+++ sphere.frag.glsl
@@ -1,3 +1,14 @@
+vec3      iResolution;           // viewport resolution (in pixels)
+float     iTime;                 // shader playback time (in seconds)
+float     iTimeDelta;            // render time (in seconds)
+float     iFrameRate;            // shader frame rate
+int       iFrame;                // shader playback frame
+float     iChannelTime[4];       // channel playback time (in seconds)
+vec3      iChannelResolution[4]; // channel resolution (in pixels)
+vec4      iMouse;                // mouse pixel coords. xy: current (if MLB down), zw: click
+vec4      iDate;                 // (year, month, day, time in seconds)
+float     iSampleRate;           // sound sample rate (i.e., 44100)
+
 // vim: ft=c commentstring=//%s
 
 // point inside triangle test
