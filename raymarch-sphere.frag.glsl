173ca6aec011d89be7571c394ccb6fbddfe51072
siers <wimuan@gmail.com>
2023-11-12 13:00:03 +0200

    light scrap

--- raymarch-sphere.frag.glsl
+++ raymarch-sphere.frag.glsl
@@ -17,11 +17,15 @@ float sdSphereAngle(vec3 p, vec3 offset, float radius) {
   return dot(normalize(p), normalize(offset - p));
 }
 
+float sdSphereLight(vec3 p, vec3 offset, float radius, vec3 light) {
+  // p - offset is sphere's normal
+  return max(0., dot(normalize(p - offset), normalize(light - p)));
+}
+
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
   vec3 uv;
-  uv = normalize(vec3(2.0 * (fragCoord.xy - iResolution.xy / 2.) / iResolution.y, 1.3));
-  vec3 light = normalize(-vec3(1, 1, 1));
+  uv = normalize(vec3(2.0 * (fragCoord.xy - iResolution.xy / 2.) / iResolution.y, 1.0));
 
   float tc = (cos(iTime * 1.7) + 1.) / 2.;
 
@@ -39,13 +43,22 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
     if (a < t || b < t) break;
   }
 
+  vec3 light = normalize(vec3(0, 2, 0));
+  /* vec3 light = normalize(vec3(0.5, 0.5, -2)); */
   vec4 red = vec4(0.92, 0.33, 0.20, 0);
   vec4 orange = vec4(1.00, 0.85, 0.79, 0);
   vec4 white = vec4(1);
 
   if (a < 3. || b < 3.) { // no idea what this 3. represents
-    uv = cross(normalize(uv), light); // THIS MAKES NO SENSE, but haven't fixed yet
-    fragColor = mix(red, white, pow(max(sdSphereAngle(uv, off1, r),sdSphereAngle(uv, off2, r)), 1.));
+    /* uv = cross(normalize(uv), light); */
+    /* fragColor = mix(red, white, pow(max(sdSphereAngle(uv, off1, r),sdSphereAngle(uv, off2, r)), 1.)); */
+
+    /* float angle = max(sdSphereAngle(uv, off1, r),sdSphereAngle(uv, off2, r)); */
+    /* float angle = sdSphereLight(uv, off1, r, light); */
+    float angle = max(sdSphereLight(uv, off1, r, light), sdSphereLight(uv, off2, r, light));
+
+    fragColor = mix(red, white, angle);
+    /* fragColor = mix(red, white, pow(angle, 1.)); */
   } else
     fragColor = vec4(1.00, 0.90, 1.00, 0);
 }
