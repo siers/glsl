72c422dbbeb8cc8593226127f0264537614734d8
siers <wimuan@gmail.com>
2023-11-17 20:43:30 +0200

    raymarch-sphere: single sdf for two spheres

--- raymarch-sphere.frag.glsl
+++ raymarch-sphere.frag.glsl
@@ -17,8 +17,23 @@ float sdSphere(vec3 p, vec3 offset, float radius) {
   return length(offset - p) - radius;
 }
 
-float sdSphereAngle(vec3 p, vec3 center, vec3 light, float radius) {
-  return clamp(dot(normalize(light - p), normalize(p - center)), 0., 1.);
+float sdf(vec3 uv) {
+  float t, c = 1.5, r = 0.4;
+  t = iTime * c;
+  vec3 center1 = vec3(0, 0, 5.) + vec3(cos(t)*2., sin(t*2.), 0);
+  t = (iTime + (.8/c)) * c;
+  vec3 center2 = vec3(0, 0, 5.) + vec3(cos(t)*2., sin(t*2.), 0);
+
+  return min(sdSphere(uv, center1, r), sdSphere(uv, center2, r));
+}
+
+float sdfLight(vec3 uv, vec3 light) {
+  float e = 0.01;
+  vec3 normal;
+  normal.x = sdf(uv + vec3(e, 0, 0)) - sdf(uv + vec3(-e, 0, 0));
+  normal.y = sdf(uv + vec3(0, e, 0)) - sdf(uv + vec3(0, -e, 0));
+  normal.z = sdf(uv + vec3(0, 0, e)) - sdf(uv + vec3(0, 0, -e));
+  return clamp(dot(normalize(light - uv), normalize(normal)), 0., 1.);
 }
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
@@ -26,31 +41,17 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
   vec3 uv = normalize(vec3(2.0 * (fragCoord.xy - iResolution.xy / 2.) / iResolution.y, 3.));
   vec3 light = vec3(0, 0, 4.5);
 
-  float t, c = 1.5;
-  t = iTime * c;
-  vec3 center1 = vec3(0, 0, 5.) + vec3(cos(t)*2., sin(t*2.), 0);
-  t = (iTime + (.8/c)) * c;
-  vec3 center2 = vec3(0, 0, 5.) + vec3(cos(t)*2., sin(t*2.), 0);
-
-  float r = 0.4;
-  float a = 100., b = 100., esc = 10.;
-  vec3 uva = uv, uvb = uv;
+  float d = 100., b = 100., esc = 10.;
 
   for (int i = 0; i < 100; i++) {
-    a = sdSphere(uva, center1, r); uva += normalize(uva) * a * 0.5;
-    b = sdSphere(uvb, center2, r); uvb += normalize(uvb) * b * 0.5;
+    d = sdf(uv);
+    uv += normalize(uv) * d * 0.5;
 
-    if (min(a, b) > esc) break;
+    if (d > esc) break;
   }
 
   vec4 red = vec4(0.92, 0.13, 0.07, 0);
   vec4 white = vec4(1);
 
-  bool first = uvb.z > uva.z;
-  float n     = first ? a : b;
-  uv          = first ? uva : uvb;
-  vec3 center = first ? center1 : center2;
-
-  float angle = sdSphereAngle(uv, center, light, r);
-  fragColor = n > esc ? vec4(0) : mix(red, white, angle);
+  fragColor = d > esc ? vec4(0) : mix(red, white, sdfLight(uv, light));
 }
